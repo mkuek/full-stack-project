2bdd26592f1197b394ed352590e87148e5b149b1
matthewvolny <matthew.volny@gmail.com>
2022-02-18 17:58:48 -0500

    added paper airplane and chat balloon images to dashboard

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,10 @@
 const characterImages = document.querySelectorAll(".image-container img");
 
 characterImages.forEach((character) => {
+  const smallCharacterImage = document.querySelector(".small-character-image");
   character.addEventListener("click", (e) => {
+    selectedCharacter = e.target.src;
     console.log("clicked");
+    smallCharacterImage.innerHTML = `<img class="image4" src="${selectedCharacter}" alt="">`;
   });
 });
