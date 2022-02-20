63bed1e2a7843805fde12c8aa81a9ab9d8747b1e
matthewvolny <matthew.volny@gmail.com>
2022-02-19 22:16:16 -0500

    minor changes, deleted code not used

--- modules/manageUsers.js
+++ modules/manageUsers.js
@@ -1,6 +1,3 @@
-//imagine that this is a list of users in the room (not sure about this one yet)
-const users = [];
-
 //join user to chat
 const userJoinObject = (id, username, roomID) => {
   const user = { id, username, roomID };
